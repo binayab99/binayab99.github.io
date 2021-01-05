00bf6711f89e133aca41e234e626ead5548f456b
Binaya Bajracharya <17489378+binayab99@users.noreply.github.com>
2021-01-05 22:33:29 +0545

    Update

---
+++ Responsive Website/responsive.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+  const hamburger = document.querySelector("hamburger");
+  const nav = document.querySelector("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+const app = () => {
+  navSlide();
+};
