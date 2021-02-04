7252fb4a26ce5c9f367c049d9f3c002a593a176e
Binaya Bajracharya <17489378+binayab99@users.noreply.github.com>
2021-02-04 23:13:37 +0545

    Update

--- Easybank/app/js/script.js
+++ Easybank/app/js/script.js
@@ -1,6 +1,7 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
@@ -13,7 +14,9 @@ btnHamburger.addEventListener("click", function () {
   } else {
     // Open Hamburger Menu
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
